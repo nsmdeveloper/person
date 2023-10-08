638a96d06950d46cf29355dca2292be59d861b98
mrtv66 <66mrtv@gmail.com>
2023-10-08 12:39:41 -0400

    Initial Commit

---
+++ src/Classes/Person.php
@@ -0,0 +1,40 @@
+<?php
+namespace Nsm\Classes;
+class Person {
+	
+	private string $fname = "";
+	private string $lname = "";
+	private string $dbirth = "";
+
+	public function setFname($fname) 
+	{
+		$this->fname = $fname;
+	}
+
+	public function setLname($lname) 
+	{
+		$this->lname = $lname;
+	}
+
+	public function setDbirth($dbirth)
+	{
+		$this->dbirth = $dbirth;
+	}
+
+	public function getFname() : string
+	{
+		return $this->fname;
+	}
+
+	public function getLname() : string
+	{
+		return $this->lname;
+	}
+
+
+	public function getDbirth() : string
+	{
+		return $this->dbirth;
+	}
+
+}
